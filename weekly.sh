d3e3fdf5eb46fadd28a0307f45eb9eb972f518a5
Byron Niblett <bniblett@gmail.com>
2023-08-22 13:24:35 -0400

    Initial Weekly Script

---
+++ weekly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# load environment file
+source .env
+
+# Build File Name
+FILE_NAME=$DB_NAME-$DATE_FORMAT".sql.gz"
+
+# Print message
+echo "Beginning MySQL Dump"
+
+# Print message
+echo "Completing MySQL Dump"
+
+
+
+
+
+# Print message
+echo "Searching for old Archives"
+
+# Print message
+echo "Completing Archive Search"
+
+# Exit Script
+exit 0
