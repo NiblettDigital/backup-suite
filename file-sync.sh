5cc25ab13ff64441dbbb640d0afe1da9bd1a00eb
Byron Niblett <bniblett@gmail.com>
2023-08-22 17:42:04 -0400

    Update Scripts to allow multiple archive syncs

--- file-sync.sh
+++ file-sync.sh
@@ -6,8 +6,17 @@ source .env
 # Print message
 echo "Beginning rsync";
 
-# rsync pull to this server 
-rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_ARCHIVE $BACKUPS/$BACKUPS_DAILY
+# rsync archive folder(s) - pull to this server 
+[ ! -z "$ARCHIVE_0" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_0/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_0;
+[ ! -z "$ARCHIVE_1" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_1/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_1;
+[ ! -z "$ARCHIVE_2" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_2/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_2;
+[ ! -z "$ARCHIVE_3" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_3/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_3;
+[ ! -z "$ARCHIVE_4" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_4/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_4;
+[ ! -z "$ARCHIVE_5" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_5/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_5;
+[ ! -z "$ARCHIVE_6" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_6/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_6;
+[ ! -z "$ARCHIVE_7" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_7/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_7;
+[ ! -z "$ARCHIVE_8" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_8/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_8;
+[ ! -z "$ARCHIVE_9" ] && rsync -arvz -e 'ssh -p '$REMOTE_PORT --delete $REMOTE_HOST:$REMOTE_PATH/$ARCHIVE_9/* $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_9;
 
 # Print message
 echo "Completing rsync";
--- setup.sh
+++ setup.sh
@@ -9,6 +9,16 @@ mkdir -p $BACKUPS/$BACKUPS_DAILY
 mkdir -p $BACKUPS/$BACKUPS_WEEKLY
 mkdir -p $BACKUPS/$BACKUPS_MONTHLY
 mkdir -p $BACKUPS/$BACKUPS_MYSQL
+[ ! -z "$ARCHIVE_0" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_0;
+[ ! -z "$ARCHIVE_1" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_1;
+[ ! -z "$ARCHIVE_2" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_2;
+[ ! -z "$ARCHIVE_3" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_3;
+[ ! -z "$ARCHIVE_4" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_4;
+[ ! -z "$ARCHIVE_5" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_5;
+[ ! -z "$ARCHIVE_6" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_6;
+[ ! -z "$ARCHIVE_7" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_7;
+[ ! -z "$ARCHIVE_8" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_8;
+[ ! -z "$ARCHIVE_9" ] && mkdir -p $BACKUPS/$BACKUPS_RSYNC/$ARCHIVE_9;
 
 # Print message
 echo "Folders Created"
