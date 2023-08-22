743b20d62ca525a2df7d3bb58c6bdcb593332f9c
Byron Niblett <bniblett@gmail.com>
2023-08-22 12:54:46 -0400

    File Sync Created

---
+++ file-sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# load environment file
+source .env
+
+# Print message
+echo "Beginning rsync";
+
+# rsync pull to this server 
+rsync -arvz -e 'ssh -p 344' --delete $REMOTE_HOST:$REMOTE_ARCHIVE $BACKUPS/$BACKUPS_DAILY
+
+# Print message
+echo "Completing rsync";
+
+# Exit Script
+exit 0;
