a39b8d3437e31343399c701c8f7e3216012ae6a3
Byron Niblett <bniblett@gmail.com>
2023-08-22 23:27:35 -0400

    Finalize new Scripts

--- mysql_2.sh
+++ mysql_2.sh
@@ -3,14 +3,17 @@
 # load environment file
 source .env
 
+# IF there isn't a second Database to archive
+[ -z "$DB_NAME_2" ] && exit 0;
+
 # Build File Name
-FILE_NAME=$DB_NAME_0-$DATE_FORMAT".sql.gz"
+FILE_NAME=$DB_NAME_2-$DATE_FORMAT".sql.gz"
 
 # Print message
 echo "Beginning MySQL Dump"
 
 # SSH into the server, complete a mysql dump and download the file
-ssh $REMOTE_HOST -p $REMOTE_PORT "mysqldump --no-tablespaces --databases $DB_NAME_0 --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD | gzip -9"  > $BACKUPS/$BACKUPS_MYSQL/$FILE_NAME
+ssh $REMOTE_HOST -p $REMOTE_PORT "mysqldump --no-tablespaces --databases $DB_NAME_2 --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD | gzip -9"  > $BACKUPS/$BACKUPS_MYSQL/$FILE_NAME
 
 # Print message
 echo "Completing MySQL Dump"
